be27438187ad54c199d630323dd2c39673dce66f
paulcjy <paulcjy@naver.com>
2023-11-19 18:53:51 +0900

    feat: Add Not-found page
    - 404 페이지 추가
    - 블로그 글 목록에서 예제에 있던 PostCard 컴포넌트 삭제

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,35 +1,10 @@
-import Link from 'next/link'
-import { compareDesc, format, parseISO } from 'date-fns'
+import { compareDesc } from 'date-fns'
 import { allPosts, Post } from 'contentlayer/generated'
 import { getMenu } from '#/lib/menu'
+import { PostCard } from '#/components/postCard'
 
-function PostCard(post: Post) {
-  return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-        >
-          {post.title}
-        </Link>
-      </h2>
-      <time
-        dateTime={post.created}
-        className="mb-2 block text-xs text-gray-600"
-      >
-        {format(parseISO(post.created), 'LLLL d, yyyy')}
-      </time>
-      <div
-        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      />
-    </div>
-  )
-}
-
-export default function Home() {
-  const posts = allPosts.sort((a, b) =>
+export default () => {
+  const posts: Post[] = allPosts.sort((a, b) =>
     compareDesc(new Date(a.created), new Date(b.created))
   )
   const menu = getMenu()
---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+export const generateMetadata = () => ({ title: '404: Not Found' })
+
+export default () => {
+  return (
+    <div className="flex flex-col h-[90vh] items-center justify-center">
+      <div className="flex items-center">
+        <h1 className="inline-block text-2xl pr-8 mr-8 border-r-[1.5px] border-gray-400 py-2">
+          404
+        </h1>
+        <h2 className="inline-block text-lg">This page could not be found</h2>
+      </div>
+    </div>
+  )
+}
