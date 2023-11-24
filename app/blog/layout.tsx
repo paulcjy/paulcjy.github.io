337b7de83639683a556ea039370d7bfcff63d2c7
paulcjy <paulcjy@naver.com>
2023-11-24 23:41:42 +0900

    style: Widen menu column for blog

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -8,7 +8,7 @@ export default function BlogLayout({
 }) {
   return (
     <div className="my-10 grid grid-cols-[1fr,minmax(auto,1100px),1fr]">
-      <div className="col-start-2 grid grid-cols-[minmax(auto,200px),min(700px,100%)] gap-x-10">
+      <div className="col-start-2 grid grid-cols-[minmax(auto,210px),min(700px,100%)] gap-x-10">
         <div>
           <BlogMenu />
         </div>
