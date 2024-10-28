ed3026ec09367d57f6fddf3a5a1206e3d96b44e4
paulcjy <paulcjy@naver.com>
2024-10-28 15:13:09 +0900

    design: 헤더 좌우 패딩 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import { headerNavigation } from '@/data/header-navigation'
 export const Header = () => {
   return (
     <header className="sticky top-0 w-full bg-white/80 shadow backdrop-blur-sm">
-      <div className="mx-auto flex h-16 max-w-8xl items-center justify-between">
+      <div className="mx-auto flex h-16 max-w-8xl items-center justify-between px-4">
         <Link href="/">
           <h1 className="text-xl font-bold">최재영의 개발 일지</h1>
         </Link>
         {/* TODO: 검색창 추가(새 컴포넌트로) */}
         <nav>
-          <ul className="font-quicksand flex space-x-6 font-medium">
+          <ul className="flex space-x-6">
             {headerNavigation.map((item) => (
               <li key={item.title}>
                 <Link href={item.href}>{item.title}</Link>
