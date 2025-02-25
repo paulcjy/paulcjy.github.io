400631af153b2382355304c5b585522c6f221619
paulcjy <paulcjy@naver.com>
2025-02-25 22:58:15 +0900

    feat: Prettier 적용 및 포맷팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "최재영의 개발 일지",
-  description: "%s | 최재영의 개발 일지",
-};
+  title: '최재영의 개발 일지',
+  description: '%s | 최재영의 개발 일지',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
