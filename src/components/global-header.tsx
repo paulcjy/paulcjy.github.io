4fb5727495cbf40efdee633fa9720a8c87a5cb0f
paulcjy <paulcjy@naver.com>
2025-03-14 01:11:03 +0900

    refactor: 사이트 제목을 h1에서 div 태그로 변경

--- src/components/global-header.tsx
+++ src/components/global-header.tsx
@@ -7,7 +7,7 @@ export const GlobalHeader = () => {
     <header className="sticky top-0 z-50 w-full bg-white/10 shadow-xs backdrop-blur-sm dark:bg-black/10 dark:shadow-white/10">
       <div className="h-global-header-height flex items-center justify-between px-8">
         <Link href="/">
-          <h1 className="text-lg font-bold">최재영의 개발 일지</h1>
+          <div className="text-lg font-semibold">최재영의 개발 일지</div>
         </Link>
         {/* TODO: 검색창 추가(새 컴포넌트로) */}
         <nav className="flex gap-4">
