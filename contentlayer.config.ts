5af183bef7b428a2c5055bbcbc7b12aed3b5af1e
paulcjy <paulcjy@naver.com>
2025-03-16 23:26:06 +0900

    rehype-autolink-headings 적용
    링크를 클릭하면 헤딩을 스크린의 제일 위로 보낸다.
    나는 sticky 헤더를 사용하고 있어서 헤딩이 헤더에 가리는 문제가 발생했다.
    해결 방법은 2가지가 있다.
    - html에 scroll-heading-top 속성을 준다.
    - 모든 h 태그에 scroll-margin-top을 준다.
    나는 html에 scroll-heading-top을 설정해서 해결했다.

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,6 +2,10 @@ import fs from 'node:fs'
 import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
 import { Post as PostType } from 'contentlayer/generated'
 import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings, {
+  Options as RehypeAutolinkHeadingsOptions,
+} from 'rehype-autolink-headings'
+import { h } from 'hastscript'
 import rehypePrettyCode, {
   Options as RehypePrettyCodeOptions,
 } from 'rehype-pretty-code'
@@ -30,11 +34,39 @@ const rehypePrettyCodeOptions: RehypePrettyCodeOptions = {
   },
 }
 
+const rehypeAutolinkHeadingsOptions: RehypeAutolinkHeadingsOptions = {
+  properties: {
+    className: ['anchor'],
+  },
+  content: (node) =>
+    h(
+      'svg',
+      {
+        className: ['octicon', 'octicon-link'],
+        viewBox: '0 0 16 16',
+        version: '1.1',
+        width: 16,
+        height: 16,
+        ariaHidden: 'true',
+      },
+      [
+        h('path', {
+          fillRule: 'evenodd',
+          d: 'M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z',
+        }),
+      ],
+    ),
+}
+
 export default makeSource({
   contentDirPath: 'posts',
   documentTypes: [Post],
   mdx: {
-    rehypePlugins: [rehypeSlug, [rehypePrettyCode, rehypePrettyCodeOptions]],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, rehypeAutolinkHeadingsOptions],
+      [rehypePrettyCode, rehypePrettyCodeOptions],
+    ],
   },
   onSuccess: async (importData) => {
     const { allPosts } = await importData()
