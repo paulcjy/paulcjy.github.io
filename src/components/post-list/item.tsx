526685a4d0e7906150db5327de09e49c26a5bf56
paulcjy <paulcjy@naver.com>
2025-03-18 19:24:41 +0900

    post item 디자인 변경
    - 링크 색상 변경
    - 날짜 색상을 고정색에서 muted-foreground 변수로 변경

--- src/components/post-list/item.tsx
+++ src/components/post-list/item.tsx
@@ -3,13 +3,17 @@ import Link from 'next/link'
 
 export const Item = ({ post }: { post: Post }) => {
   const { title, slug, created } = post
+
   return (
     <article className="mb-8">
       <Link href={`/blog/posts/${slug}`}>
-        <h2 className="mb-1 text-xl font-bold text-emerald-600 transition-all duration-200 hover:text-red-700/70">
+        <h2 className="mb-1 text-xl font-bold text-emerald-600/80 transition-all duration-200 hover:text-red-600/50 dark:text-emerald-400 dark:hover:text-red-400">
           {title}
         </h2>
-        <time dateTime={created} className="mb-2 block text-sm text-gray-600">
+        <time
+          dateTime={created}
+          className="text-muted-foreground mb-2 block text-sm"
+        >
           {new Date(created).toLocaleDateString()}
         </time>
         {/* {description && <p>{description}</p>} */}
